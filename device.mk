512bd9dbb33b6dd7b082aff0b18b54b420c5e8ce
root2raj <ruturaj1999@gmail.com>
2021-03-15 23:35:39 +0530

    whyred: Lineage-fy

--- device.mk
+++ device.mk
@@ -61,7 +61,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-pe
+    $(DEVICE_PATH)/overlay-lineage
 
 # Powerhint
 EAS_POWERHINT_VARIANT := sdm636
